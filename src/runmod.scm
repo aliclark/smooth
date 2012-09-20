62867d30a562b94963c37e9ff915f92d157a2809
Ali Clark <ali@clark.gb.net>
2012-09-20 18:32:29 +0100

    small amount of commandline checking in runmod

--- src/runmod.scm
+++ src/runmod.scm
@@ -1,4 +1,14 @@
 #!/usr/bin/gsi-script
 
-(load (cadr (command-line)))
-(start)
+(define (runmod)
+  (let ((c (command-line)))
+    (if (< (length c) 2)
+      (begin
+        (display "Nothing to do")
+        (newline))
+      (begin
+        (load (cadr c))
+        ; all input must be read from stdin
+        (start)))))
+
+(runmod)
