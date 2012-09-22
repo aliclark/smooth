98ab66b9fde25f41af55d4dcb9e24882ca89479b
Ali Clark <ali@clark.gb.net>
2012-09-22 17:11:09 +0100

    moving useful smooth/scheme code into src dir

---
+++ src/basicio.scm
@@ -0,0 +1,38 @@
+
+(define iocons      (extern io_iocons))
+(define iocar       (extern io_iocar))
+(define iocdr       (extern io_iocdr))
+(define stdin       (extern io_stdin))
+(define stdout      (extern io_stdout))
+(define fgetb       (extern io_fgetb))
+(define fputb       (extern io_fputb))
+(define inttochurch (extern io_inttochurch))
+(define churchtoint (extern io_churchtoint))
+
+(define (return x) (lambda (r) (iocons x r)))
+
+(define (>>= m)
+  (lambda (f)
+    (lambda (r)
+      (let ((v1 (m r)))
+        ((f (iocar v1)) (iocdr v1))))))
+
+(define (make->> f)
+  (lambda (m1)
+    (lambda (m2)
+      ((f m1) (lambda (x) m2)))))
+
+(define >> (make->> >>=))
+
+(define getb (fgetb stdin))
+
+;; somehow the problem happens in here...
+(define putb (fputb stdout))
+
+(define getc
+  ((>>= getb)
+    (lambda (c)
+      (return (inttochurch c)))))
+
+(define (putc c)
+  (putb (churchtoint c)))
