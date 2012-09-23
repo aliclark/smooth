1f3802a298353582f75c5299c74a53e3f77efdd7
Ali Clark <ali@clark.gb.net>
2012-09-23 17:49:56 +0100

    adding comments to build script to help infer duration of compile steps

--- build-comments.sh
+++ build-comments.sh
@@ -20,17 +20,33 @@ srcfile=comments.smc
 deps="lambda.scm bool.scm pair.scm numeral.scm list.scm basicio.scm $srcfile"
 
 for f in $deps; do
-    echo "precompiling dependency $f to" `echo $f | sed s/\.smc$/\.smo/ | sed s/\.scm$/\.smo/` >&2
-    gsi ../target/comments.scm <$f | ./runmod.scm sexpr-to-parseobj.scm | ./runmod.scm macexpand.scm > `echo $f | sed s/\.smc$/\.smo/ | sed s/\.scm$/\.smo/`
+    fout=`echo $f | sed s/\.smc$/\.smo/ | sed s/\.scm$/\.smo/`
+    echo "precompiling dependency $f to $fout" >&2
+    echo "  comment stripping" >&2
+    gsi ../target/comments.scm <$f >$fout.1
+    echo "  creating parse objects" >&2
+    ./runmod.scm sexpr-to-parseobj.scm <$fout.1 >$fout.2
+    rm $fout.1
+    echo "  macro expansion" >&2
+    ./runmod.scm macexpand.scm <$fout.2 > $fout
+    rm $fout.2
 done
 
 srcpre=`echo $f | sed s/\.smc$/\.smo/ | sed s/\.scm$/\.smo/`
+targ=../target/`echo $srcpre | sed s/\.smo$/\.scm/`
 
 # .smo files are ready to be load'ed
-echo "compiling srcfile $srcfile" >&2
-./runmod.scm load.scm <$srcpre | ./runmod.scm varexpand.scm | ./runmod.scm output-scm.scm >../target/`echo $srcpre | sed s/\.smo$/\.scm/`.out
+echo "compiling srcfile $srcfile to $targ.out" >&2
+echo "  loading included files" >&2
+./runmod.scm load.scm <$srcpre >$targ.out.1
+echo "  expanding main variable" >&2
+./runmod.scm varexpand.scm <$targ.out.1 >$targ.out.2
+rm $targ.out.1
+echo "  outputting target code" >&2
+./runmod.scm output-scm.scm <$targ.out.2 >$targ.out
+rm $targ.out.2
 
 # Only do this once you are sure it is correct, or the previous version is well backed up.
 # It would be a pita to lose a working copy of the program.
 
-#mv ../target/`echo $srcpre | sed s/\.smo$/\.scm/`.out `echo $srcpre | sed s/\.smo$/\.scm/`
+#mv $targ.out $targ
