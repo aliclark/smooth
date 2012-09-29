08524b00747d360d1c3f1639d5be9e6d68dcd0fb
Ali Clark <ali@clark.gb.net>
2012-09-29 17:03:29 +0100

    adding a pass that can convert lambda expressions to de brujin index format
    This is helpful for determining alpha equivalence of expressions.
    
    The pass is currently not used anywhere, but will be used in
    dedeplication later on.

---
+++ src/debrujin.scm
@@ -0,0 +1,55 @@
+
+(load "base.scm")
+
+;; If the lambda is contained (not a closure) it can be
+;; compared with any other contained expression.
+
+;; If a lambda is a closure (references variables at higher scopes),
+;; then any variables at higher scopes must be the same ones
+;; for any two lambda expressions to be equal.
+;; If the depth of the closure changes with respect to the closed variables,
+;; then this will not cause a problem for Scheme, but may do for C.
+;;
+;; eg (lamba (3 0)) and (lambda (2 0)) where the former is
+;; referring to the same closed variable as the latter,
+;; but has higher index because it is within an extra lambda.
+
+;; (lambda x (lambda y (somenative (lambda z (z x y)) y)))
+;;   leads to:
+;; (lambda (lambda (somenative (lambda (0 1 2)) 1)))
+
+;; Look through with an assoc-list of (varname count)
+;; whenever a lambda is encountered, add it with count 0,
+;; replacing a previous count if it shadows, map the other counts doing +1,
+;; and call recursively inside with this new assoc-list.
+;; When the call ends this assoc-list argument is naturally discarded.
+
+(define (debrujin px vari)
+  (let ((x (parseobj-obj px)))
+    (if (list? x)
+      (if (reserved-form-type? px '__lambda__ 3)
+        (parseobj-sel 1
+          (lambda (v) (parseobj-mk '__debrujin__ '()))
+          (parseobj-sel 2
+            (lambda (exp)
+              (debrujin exp
+                (cons (list (parseobj-obj (cadr x)) 0)
+                  (map (lambda (p) (list (car p) (+ (cadr p) 1))) vari))))
+            px))
+        (parseobj-mk (map (lambda (x) (debrujin x vari)) x) (parseobj-props px)))
+      (parseobj-mk (number->symbol (assoc-ref vari x #f)) (parseobj-props px)))))
+
+(define parse-phase-debrujin
+  (parseobj-convf
+    (lambda (xs)
+      (map
+        (lambda (px)
+          (if (reserved-form-type? px '__start__ 2)
+            (parseobj-sel 1 (lambda (x) (debrujin x '())) px)
+            px))
+        xs))))
+
+(define (start)
+  (parse-output-to-port (current-output-port)
+    (parse-phase-debrujin
+      (read-sexprs-from-port (current-input-port)))))
